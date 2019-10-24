650b57246a085dc0a3eb67e6b640931c7a1d4acf
Ishar-Menon <isharmenon99@gmail.com>
2019-10-24 19:56:54 +0530

    Modified client and server to support full duplex communication

--- client.py
+++ client.py
@@ -1,23 +1,84 @@
-from socket import *
+import socket
+import select
+import errno
+import sys
+import threading 
+
+HEADER_LENGTH = 10
+IP = "127.0.0.1"
+PORT = 1234
+
+my_username = input("Username: ")
+
+client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client_socket.connect((IP,PORT))
+client_socket.setblocking(False)
+
+username = my_username.encode('utf-8')
+username_header = f"{len(username):<{HEADER_LENGTH}}".encode("utf-8")
+client_socket.send(username_header + username)
 
 
+sendMessageActive = 0
+getMessageActive = 0
 
-# server info
-serverName = '192.168.43.179'
-serverPort = 11000
+def getMessage():
 
-# create client socket 
-clientSocket = socket(AF_INET,SOCK_STREAM)
-# create tcp connection with server 
-clientSocket.connect((serverName,serverPort))
+    global getMessageActive
 
-# Take input from user 
-sentence = input("Input lowercase letter : ")
-clientSocket.send(str.encode(sentence))
+    try:
+        while True:
+            username_header = client_socket.recv(HEADER_LENGTH)
+            if not len(username_header):
+                print("connection closed by the server")
+                sys.exit()
+            username_length = int(username_header.decode("utf-8").strip())
+            username = client_socket.recv(username_length).decode("utf-8")
 
-returnMessage = clientSocket.recv(1024)
+            message_header = client_socket.recv(HEADER_LENGTH)
+            message_length = int(message_header.decode("utf-8").strip())
+            message = client_socket.recv(message_length).decode("utf-8")
 
-print(returnMessage.decode())
+            print(f"{username} > {message}")
 
-clientSocket.close()
+    except IOError as e:
+        # If there is nothing left to read then just continue
+        if(e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK):
+            print('Reading error',str(e))
+        
 
+    except Exception as e:
+        print('General error',str(e))
+        sys.exit()
+
+    getMessageActive = 0
+def sendMessage():
+
+    global sendMessageActive
+
+    message = input(f"{my_username} > ")
+
+    if(message):
+        message = message.encode("utf-8")
+        message_header = f"{len(message) :< {HEADER_LENGTH}}".encode("utf-8")
+        client_socket.send(message_header + message)
+    sendMessageActive = 0
+
+while True:
+    if(not sendMessageActive):
+        sendMessageActive = 1
+        sendMsgThread = threading.Thread(target=sendMessage)
+        sendMsgThread.start()
+    
+    if(not getMessageActive):
+        getMessageActive = 1
+        getMsgThread = threading.Thread(target=getMessage)
+        getMsgThread.start()
+
+    
+
+
+        
+
+    
+        
